ad328fd42e2953cf1e8e68600756179c46fb564f
Nickolas Santiago <choragosdesigns@gmail.com>
2017-05-11 18:07:48 -0700

    Add files via upload

--- scripts/project_blurb_script.js
+++ scripts/project_blurb_script.js
@@ -7,6 +7,30 @@ function test(t)
 function drawSelectedProject(all_projects_array, project_index)
 {
     var selected_project_html = "";
+    selected_project_html += "<div class='featured_project_main_content'>";
+    selected_project_html += "<div class='featured_project_description'>";
+    selected_project_html += "<div class='pasta'>";
+    selected_project_html += "<p class='featured_project_title'>" + all_projects_array[project_index].name + "</p>";
+    selected_project_html += "<p class='featured_project_dates'>" + all_projects_array[project_index].dates + "</p>";
+    for(var p = 0; p < all_projects_array[project_index].description.length; p++)
+    {
+        selected_project_html += "<p class='featured_project_description_text'>" + all_projects_array[project_index].description[p] + "</p>";
+    }
+    selected_project_html += "<p class='featured_project_description_button'>SHOW MORE</p>"
+    selected_project_html += "</div>";
+    selected_project_html += "</div>";
+    selected_project_html += "<div class='featured_project_image_gallery'>";
+    selected_project_html += "<img class='featured_project_image_gallery_img' id='selected_project_img' src='" + all_projects_array[project_index].imgs[0].src + "'/>";
+    selected_project_html += "<div class='featured_project_image_gallery_icon_wrapper'>";
+    selected_project_html += drawImageGalleryIcons(all_projects_array[project_index].imgs, 0);
+    selected_project_html += "</div>";
+    selected_project_html += "</div>";
+    selected_project_html += "</div>";
+    selected_project_html += "</div>";
+    selected_project_html += "<p id='more_projects_button'><u>Go Back</u></p>"
+    return selected_project_html;
+    /*
+    var selected_project_html = "";
     selected_project_html += "<div class='project_blurb'>";
     selected_project_html += "<div class='project_description'>";
     selected_project_html += "<div class='project_description_content'>";
@@ -27,7 +51,7 @@ function drawSelectedProject(all_projects_array, project_index)
     selected_project_html += "</div>";
     selected_project_html += "</div>";
     selected_project_html += "</div>";
-    return selected_project_html;
+    return selected_project_html;*/
 }
 function drawImageGalleryIcons(set_images, selected_image)
 {
