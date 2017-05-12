7a76f5c5dbb0176aeff409271d67c38a65fa8086
Nickolas Santiago <choragosdesigns@gmail.com>
2017-05-11 18:54:16 -0700

    Add files via upload

--- scripts/project_blurb_script.js
+++ scripts/project_blurb_script.js
@@ -9,7 +9,7 @@ function drawSelectedProject(all_projects_array, project_index)
     var selected_project_html = "";
     selected_project_html += "<div class='project_content'>";
     selected_project_html += "<div class='project_description'>";
-    selected_project_html += "<div class='pasta'>";
+    selected_project_html += "<div class='project_description_cell'>";
     selected_project_html += "<p class='project_title'>" + all_projects_array[project_index].name + "</p>";
     selected_project_html += "<p class='project_dates'>" + all_projects_array[project_index].dates + "</p>";
     for(var p = 0; p < all_projects_array[project_index].description.length; p++)
