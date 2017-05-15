07124f818a2a427de6c9aab4ae0b1da77af8be10
Nickolas Santiago <choragosdesigns@gmail.com>
2017-05-14 20:25:49 -0700

    Add files via upload

--- scripts/index_scripts.js
+++ scripts/index_scripts.js
@@ -58,12 +58,13 @@ $(document).ready(function(e)
         $(".project_content").each(function(i)
         {
             console.log(i);
-            console.log($(i).find(".project_description_text"));
+            /*console.log($(i).find(".project_description_hidden_text"));
             if($(i).find(".project_description_hidden_text").length > 0)
             {
                 console.log(".project_description_text");
                 $(".project_description_cell").append("<p class='project_description_button'>SHOW MORE</p>");
-            }
+            }*/
+            console.log($(i));
         });
         
         
